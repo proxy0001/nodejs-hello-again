9a526edf5ddf1504e1a2c63f539bb53d0f00b1eb
proxy0001 <g.proxy0001@gmail.com>
2016-08-12 17:00:22 +0800

    add restful api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
+// routes/index.js
+
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// get all posts
+router.get('/1/post', function(req, res, next) {
+  console.log('get all posts');
+  res.send('get all posts');
+});
+// get post by id
+router.get('/1/post/:id', function(req, res, next) {
+  console.log('get post by id');
+  res.send('get post by id');
+});
+// new post
+router.post('/1/post', function(req, res, next) {
+  console.log('new post');
+  res.send('new post');
+});
+// modify post
+router.put('/1/post/:id', function(req, res, next) {
+  console.log('modify post');
+  res.send('modify post');
+});
+// remove post
+router.delete('/1/post', function(req, res, next) {
+  console.log('remove post');
+  res.send('remove post');
 });
 
 module.exports = router;
